102f2ed7f66c5e4c03fd954e7050a166dda21a09
Monika <dotyk_zla@o2.pl>
2023-01-28 23:02:46 +0100

    Add 2 featured categories

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,46 +1,85 @@
-import {
-	ChangeDetectionStrategy,
-	Component,
-	ViewEncapsulation,
-} from "@angular/core";
-import { map, Observable, reduce, shareReplay } from "rxjs";
-import { CategoryModel } from "../../models/category.model";
-import { StoreModel } from "../../models/store.model";
-import { StoreTagModel } from "../../models/store-tag.model";
-import { CategoriesService } from "../../services/categories.service";
-import { StoresService } from "../../services/stores.service";
-import { StoreTagsService } from "../../services/store-tags.service";
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+import { CategoryModel } from '../../models/category.model';
+import { StoreModel } from '../../models/store.model';
+import { StoreTagModel } from '../../models/store-tag.model';
+import { ProductModel } from '../../models/product.model';
+import { CategoriesService } from '../../services/categories.service';
+import { StoresService } from '../../services/stores.service';
+import { StoreTagsService } from '../../services/store-tags.service';
+import { ProductsService } from '../../services/products.service';
+import { Observable, map, reduce, shareReplay, combineLatest, tap } from 'rxjs';
+import { ProductWithCategoryQueryModel } from 'src/app/query-models/product-with-category.query-model';
 
 @Component({
-	selector: "app-home",
-	templateUrl: "./home.component.html",
-	styleUrls: ["./home.component.css"],
-	encapsulation: ViewEncapsulation.None,
-	changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-	readonly categories$: Observable<CategoryModel[]> = this._categoriesService
-		.getAll()
-		.pipe(shareReplay(1));
+  readonly categories$: Observable<CategoryModel[]> = this._categoriesService
+    .getAll()
+    .pipe(shareReplay(1));
 
-	readonly stores$: Observable<StoreModel[]> = this._storesService
-		.getAll()
-		.pipe(shareReplay(1));
+  readonly stores$: Observable<StoreModel[]> = this._storesService
+    .getAll()
+    .pipe(shareReplay(1));
 
-	readonly storeTags$: Observable<StoreTagModel[]> =
-		this._storeTagsService.getAll();
+  readonly storeTags$: Observable<StoreTagModel[]> =
+    this._storeTagsService.getAll();
 
-	readonly storeTagsMap$ = this.storeTags$.pipe(
-		map((storeTags: StoreTagModel[]) => {
-			return storeTags.reduce((a, c) => {
-				return { ...a, [c.id]: c };
-			}, {}) as Record<string, StoreTagModel>;
-		})
-	);
+  readonly storeTagsMap$ = this.storeTags$.pipe(
+    map((storeTags: StoreTagModel[]) => {
+      return storeTags.reduce((a, c) => {
+        return { ...a, [c.id]: c };
+      }, {}) as Record<string, StoreTagModel>;
+    })
+  );
+  readonly productsWithCategory$: Observable<ProductWithCategoryQueryModel[]> = combineLatest([
+    this._productsService.getAll(),
+    this.categories$
+  ]).pipe(
+    map(([products, categories]:[ProductModel[], CategoryModel[]]) => {
+      const categoryMap = categories.reduce((a, c) => {
+          return { ...a, [c.id]: c };
+        }, {}) as Record<string, CategoryModel>;
 
-	constructor(
-		private _categoriesService: CategoriesService,
-		private _storesService: StoresService,
-		private _storeTagsService: StoreTagsService
-	) {}
+      return products.map((product) => ({
+        ...product,
+        categoryName: categoryMap[product.categoryId].name
+      }))
+    })
+  )
+
+
+  readonly productsFruitAndVegetabled$: Observable<ProductWithCategoryQueryModel[]> = this.productsWithCategory$.pipe(
+    map((products:ProductWithCategoryQueryModel[]) => {
+      return products.filter(product => product.categoryName === "Fruits & Vegetables").sort((a,b) => {
+        if (a.featureValue > b.featureValue) return -1;
+        if (a.featureValue < b.featureValue) return 1 ;
+        return 0
+      }
+      ).slice(0,5)
+    },
+  ))
+      
+  readonly productSnackAndMunchies$: Observable<ProductWithCategoryQueryModel[]> = this.productsWithCategory$.pipe(
+    map((products:ProductWithCategoryQueryModel[]) => {
+      return products.filter(product => product.categoryName === "Snack & Munchies").sort((a,b) => {
+        if (a.featureValue > b.featureValue) return -1;
+        if (a.featureValue < b.featureValue) return 1 ;
+        return 0
+      }
+      ).slice(0,5)
+    },
+  ))
+
+
+  constructor(
+    private _categoriesService: CategoriesService,
+    private _storesService: StoresService,
+    private _storeTagsService: StoreTagsService, private _productsService: ProductsService
+  ) { }
 }
---
+++ src/app/query-models/product-with-category.query-model.ts
@@ -0,0 +1,13 @@
+export interface ProductWithCategoryQueryModel {
+        readonly name: string;
+        readonly price: number;
+        readonly categoryId: string;
+        readonly ratingValue: number;
+        readonly ratingCount: number;
+        readonly imageUrl: string;
+        readonly featureValue: number;
+        readonly storeIds: string[];
+        readonly id: string;
+        readonly categoryName: string;
+}
+
